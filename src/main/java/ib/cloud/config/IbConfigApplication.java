3767d309247900dd3ce99ab66335e9e14ba87531
Sanghui Park <uhaha@ncsoft.com>
2019-12-13 12:05:33 +0900

    first commit

---
+++ src/main/java/ib/cloud/config/IbConfigApplication.java
@@ -0,0 +1,13 @@
+package ib.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbConfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ib/cloud/config/IbConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ib.cloud.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
